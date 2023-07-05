e0bfefc8d1a76ca50848a9413e47191b877a0951
Pieter Deane <deanepieter@gmail.com>
2023-07-05 18:31:25 +0200

    added a title header

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,8 @@ class App extends Component {
     });
 
     return (
-      <div>
+      <div className="App">
+        <h1 className="app-title">Monsters Rolodex </h1>
         <SearchBox
           className="monsters-search-box"
           SearchBox
@@ -48,7 +49,9 @@ class App extends Component {
           placeholder="search monsters "
         />
 
-        <CardList monsters={filteredMonsters} />
+       <CardList monsters={filteredMonsters} />
+       
+      
       </div>
     );
   }
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,26 +1,24 @@
-import { Component } from "react";
+import React, { Component } from "react";
+import Card from '../card/card.component'
+import './card-list.styles.css'
 
-class cardList extends Component {
+class CardList extends Component {
   render() {
-    console.log(this.props.monsters);
-    console.log("rendering card list");
     const { monsters } = this.props;
 
     return (
       <div className="card-list">
-        {monsters.map((monster) => (
-          <div className="card-container" key={monster.id}>
-            <img
-              alt={`monster ${monster.name}`}
-              src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
-            />
-            <h2>{monster.name}</h2>
-            <p>{monster.email}</p>
-          </div>
-        ))}
+        {monsters.map((monster) => {
+        
+          return (
+            <Card monster={monster}/>
+            
+          );
+        })}
       </div>
     );
   }
 }
 
-export default cardList;
+export default CardList;
+
